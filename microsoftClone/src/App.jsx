f95d50d7bceb0cb6add61e5530eeea68729d472f
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-04-13 22:27:50 +0500

    updated some files

--- microsoftClone/src/App.jsx
+++ microsoftClone/src/App.jsx
@@ -1,6 +1,23 @@
+import Header from './sections/Header'
+import Hero from './sections/Hero'
+import Features from './sections/Features'
+import Business from './sections/Business'
+import Footer from './sections/Footer'
+import Orgainze from './sections/Organize'
+
 const App = () => {
   return (
-    <></>
+    <>
+      <header>
+        <Header />
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
   )
   
 }
--- microsoftClone/src/sections/Features.jsx
+++ microsoftClone/src/sections/Features.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Main = () => {
+const Features = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Main = () => {
   )
 }
 
-export default Main
+export default Features
