bf3508f8a34b30d5067a0b86bd3c8f5f2df58322
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-04-13 22:56:35 +0500

    made changes

---
+++ microsoftClone/src/constants/index.js
@@ -0,0 +1,46 @@
+import { feature1, feature2, feature3, feature4 } from "../assets/pics"
+
+export const navLinks = [
+    {href: '#', label: "Microsoft 365"},
+    {href: "#", label: "Office"},
+    {href: "#", label: "Copilot"},
+    {href: "#", label: "Windows"},
+    {href: "#", label: "Support"},
+]
+
+
+export const features = [
+    {
+        img: feature1,
+        heading: "Designed for life today – and tomorrow",
+        description: "The next-generation of games. Your goals. Friends and family. Windows 11 was made to bring you closer to everything you love.",
+        buttonLabel: "See if your PC is ready"
+    },
+    {
+        img: feature2,
+        heading: "Microsoft Edge",
+        description: "World-class performance with more privacy, more productivity and more value while you browse",
+        buttonLabel: "Download Now"
+    },
+    {
+        img: feature3,
+        heading: "Microsoft OneDrive",
+        description: "Save your files and photos to OneDrive and access them from any device, anywhere.",
+        buttonLabel: "Learn More"
+    },
+    {
+        img: feature4,
+        heading: "Outlook for iOS and Android",
+        description: "Connect. Organise. Get things done.",
+        buttonLabel: "Download Now"
+    }
+]
+
+export const Business = [
+    {
+        img:
+        heading: "Microsoft 365 Copilot",
+        description: "Save time and focus on the things that matter most with AI in Microsoft 365 for business.",
+        buttonLabel: "Learn More",
+    }
+]
