6b66b7c5efbd23669153cab907428d360bca0a94
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-04-14 00:31:40 +0500

    header done

---
+++ microsoftClone/src/assets/pics/index.js
@@ -0,0 +1,21 @@
+import feature1 from './feature1.jfif';
+import feature2 from './feature2.webp';
+import feature3 from './feature3.webp';
+import feature4 from './feature4.jpg';
+
+import business1 from './business1.jpg';
+import business2 from './business2.jpg';
+import business3 from './business3.webp';
+import business4 from './business4.jpg';
+
+export {
+  feature1,
+  feature2,
+  feature3,
+  feature4,
+  
+  business1,
+  business2,
+  business3,
+  business4,
+};
--- microsoftClone/src/sections/Header.jsx
+++ microsoftClone/src/sections/Header.jsx
@@ -1,7 +1,32 @@
+import logo from '../assets/pics/logo.png'
+import { navLinks } from '../constants'
+
 const Header = () => {
   return (
-    <div className="bg-white ">
-      
+    <div className="bg-white w-full flex justify-between md:justify-between lg:px-7 p-5 px-6 items-center">
+      <div className='flex w-[60%] md:w-[70%] md:justify-end gap-10 justify-between md:flex-row-reverse'>
+        <i className="fa-solid fa-bars text-2xl md:hidden"></i>
+        <div className='hidden md:flex'>
+          <ul className='flex gap-5'>
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <a className='text-[13px] hover:border-b-2 hover:border-black' href={link.href}>{link.label}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <img className='w-28' src={logo} alt="logo microsoft" />
+        </div>
+      </div>
+
+      <div className='flex justify-between gap-6 items-center'>
+        <select className='hidden md:block'>
+          <option value="">All Microsoft</option>
+        </select>
+        <i className="fa-solid fa-cart-shopping text-xl"></i>
+        <i className="fa-solid fa-user-plus text-xl"></i>
+      </div>
     </div>
   )
 }
