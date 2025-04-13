a8865e9b9d55d9013dd273118554eeccc044c01d
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-04-14 00:34:17 +0500

    header completed

--- microsoftClone/src/sections/Header.jsx
+++ microsoftClone/src/sections/Header.jsx
@@ -5,7 +5,9 @@ const Header = () => {
   return (
     <div className="bg-white w-full flex justify-between md:justify-between lg:px-7 p-5 px-6 items-center">
       <div className='flex w-[60%] md:w-[70%] md:justify-end gap-10 justify-between md:flex-row-reverse'>
+        <div className='md:hidden'>
         <i className="fa-solid fa-bars text-2xl md:hidden"></i>
+        </div>
         <div className='hidden md:flex'>
           <ul className='flex gap-5'>
             {navLinks.map((link, index) => (
