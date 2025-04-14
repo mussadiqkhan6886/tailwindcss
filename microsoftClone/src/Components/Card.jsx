faf866f7f9ed29b6385bf38b1d545e78c6d6752b
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-04-14 12:25:21 +0500

    adjusting buttons in features

--- microsoftClone/src/Components/Card.jsx
+++ microsoftClone/src/Components/Card.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import Button from "./Button"
 
-const Card = () => {
+const Card = ({img, heading, description, buttonLabel}) => {
   return (
-    <div>
-      
+    <div className="w-full shadow-xl relative">
+      <img className="w-full" src={img} alt="Feature Image" />
+      <div className="p-6 flex flex-col justify-between lg:h-[320px]">
+        <div>
+          <h2 className="text-[21px] lg:font-semibold font-semibold">{heading}</h2>
+          <p className="my-2.5">{description}</p>
+        </div>
+        <Button label={buttonLabel} />
+      </div>
     </div>
   )
 }
--- microsoftClone/src/sections/Features.jsx
+++ microsoftClone/src/sections/Features.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { features } from '../constants'
+import Card from '../Components/Card'
 
 const Features = () => {
   return (
-    <div>
-      
+    <div className='p-4 mt-3 grid grid-cols-1 gap-12 at-580px md:mt-40 at-1060px lg:gap-7 lg:px-12'>
+      {features.map((feature, index) => (
+        <Card key={index} {...feature} />
+      ))}
     </div>
   )
 }
