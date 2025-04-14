e3ec40127abc10e1043e800e8ff629b305d35219
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-04-15 00:12:37 +0500

    adjsutmnet button

--- microsoftClone/src/sections/Business.jsx
+++ microsoftClone/src/sections/Business.jsx
@@ -23,7 +23,7 @@ const Business = () => {
         </div>
       </div>
       <a href="#top" className="">
-        <button className="float-right bg-gray-300 font-semibold px-5 py-2 mr-5"><i className="fa-solid fa-arrow-up mr-2"></i>Back to top</button>
+        <button className="float-right bg-gray-300 font-semibold px-5 py-2 mr-5 hover:bg-white cursor-pointer hover:shadow-2xl"><i className="fa-solid fa-arrow-up mr-2"></i>Back to top</button>
       </a>
     </div>
     </>
