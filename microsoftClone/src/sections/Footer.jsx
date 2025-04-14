2048fb30b6c228f631e826c6831fa6b817dea0cf
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-04-15 01:03:48 +0500

    project done

--- microsoftClone/src/sections/Footer.jsx
+++ microsoftClone/src/sections/Footer.jsx
@@ -17,17 +17,17 @@ const Footer = () => {
       </div>
       <div className='bg-gray-100 flex justify-between p-7 lg:px-15'>
         <div className='flex text-[14px] text-gray-600 gap-9'>
-          <h6>English (Pakistan)</h6>
-          <h6>Your Privacy Choices</h6>
-          <h6>Consumer Health Privacy</h6>
+          <h6 className='hover:border-black border-transparent border-b-[1px] cursor-pointer'>English (Pakistan)</h6>
+          <h6 className='hover:border-black border-transparent border-b-[1px] cursor-pointer'>Your Privacy Choices</h6>
+          <h6 className='hover:border-black border-transparent border-b-[1px] cursor-pointer'>Consumer Health Privacy</h6>
         </div>
         <div className='flex text-[14px] text-gray-600 gap-7'>
-          <h6>Contact Microsoft</h6>
-          <h6>Privacy</h6>
-          <h6>Terms of use</h6>
-          <h6>Trademarks</h6>
-          <h6>About our ads</h6>
-          <h6>&copy; Microsoft 2025</h6>
+          <h6 className='hover:border-black border-transparent border-b-[1px] cursor-pointer'>Contact Microsoft</h6>
+          <h6 className='hover:border-black border-transparent border-b-[1px] cursor-pointer'>Privacy</h6>
+          <h6 className='hover:border-black border-transparent border-b-[1px] cursor-pointer'>Terms of use</h6>
+          <h6 className='hover:border-black border-transparent border-b-[1px] cursor-pointer'>Trademarks</h6>
+          <h6 className='hover:border-black border-transparent border-b-[1px] cursor-pointer'>About our ads</h6>
+          <h6 className='hover:border-black border-transparent border-b-[1px] cursor-pointer'>&copy; Microsoft 2025</h6>
         </div>
       </div>
     </>
